84b1e97807b573e9b1d462b0443fe22ede87920b
GSmiguel <102688472+GSmiguel@users.noreply.github.com>
2024-10-05 17:33:34 -0300

    Update basic_script.py

--- basic_script.py
+++ basic_script.py
@@ -10,62 +10,48 @@ email = os.getenv('BLUESKY_EMAIL')
 password = os.getenv('BLUESKY_PASSWORD')
 pexels_api_key = os.getenv('PEXELS_API_KEY')
 
-# URL base da Pexels API
 pexels_url = 'https://api.pexels.com/v1/search'
 
-# Limite de tamanho da imagem (976.56KB)
-image_size_limit = 976.56 * 1024  # em bytes
+image_size_limit = 976.56 * 1024
 
-# Lista de termos-chave para sortear
 search_terms = ['furniture', 'wood furniture', 'plastic furniture', 'metal furniture', 'glass furniture',
                 'bamboo furniture', 'ceramic furniture', 'bathroom furniture', 'kitchen furniture',
                 'restroom furniture', 'bedroom furniture', 'garden furniture', 'office furniture', 'hotel furniture']
 
-# Sorteia aleatoriamente um termo de busca
 random_term = random.choice(search_terms)
 
-# Número de resultados por página e o número de páginas consultadas
 results_per_page = 100
 max_pages = 5
 
-# Sorteia uma página aleatória para consultar
 random_page = random.randint(1, max_pages)
 
-# Parâmetros para a busca de imagens
 params = {
-    'query': random_term,       # Termo de busca sorteado
-    'per_page': results_per_page,  # Quantidade de imagens por página
-    'page': random_page          # Página aleatória
+    'query': random_term,
+    'per_page': results_per_page,
+    'page': random_page
 }
 
-# Cabeçalhos com a chave de API
 headers = {
     'Authorization': pexels_api_key
 }
 
-# Fazendo a solicitação à API do Pexels
 response = requests.get(pexels_url, headers=headers, params=params)
 
 if response.status_code == 200:
     data = response.json()
     
-    # Verifica se há imagens nos resultados
     if data['photos']:
         suitable_image_found = False
 
-        # Tenta encontrar uma imagem que esteja dentro do limite de tamanho
         while not suitable_image_found and data['photos']:
-            # Sorteia aleatoriamente uma imagem entre os resultados
             random_image = random.choice(data['photos'])
             
-            # Pega a URL da imagem sorteada
             image_url = random_image['src']['large']
             image_response = requests.get(image_url)
             
             if image_response.status_code == 200:
                 image_data = image_response.content
 
-                # Verifica o tamanho da imagem
                 image_size = len(image_data)
                 print(f"Tamanho da imagem: {image_size / 1024:.2f} KB")
 
@@ -74,35 +60,25 @@ if response.status_code == 200:
                 else:
                     print("Imagem muito grande, tentando outra...")
 
-            # Remove a imagem que foi tentada da lista
             data['photos'].remove(random_image)
         
         if suitable_image_found:
-            # Descrição da imagem
             image_alt_text = f"{random_term.capitalize()}"
 
-            # Tentando fazer login no Bluesky
             try:
-                print("Tentando fazer login...")
                 client = Client(base_url='https://bsky.social')
                 client.login(email, password)
-                print("Login realizado com sucesso!")
             except Exception as e:
-                print(f"Erro ao fazer login: {e}")
+                print(f"Login error: {e}")
 
-            # Criando e enviando o post com imagem
             try:
-                # Enviando a imagem
                 upload_response = client.send_image(image=image_data, text='', image_alt=image_alt_text)
-                
-                # Verifique a resposta do upload da imagem
-                print("Resposta do upload da imagem:", upload_response)
                     
             except Exception as e:
-                print(f"Erro ao criar o post: {e}")
+                print(f"Post creation error: {e}")
         else:
-            print("Nenhuma imagem encontrada que esteja dentro do limite de tamanho.")
+            print("No suitable image found.")
     else:
-        print("Nenhuma imagem encontrada.")
+        print("No image found.")
 else:
-    print(f"Erro ao acessar a API do Pexels: {response.status_code}")
+    print(f"Error on Pexels API: {response.status_code}")
